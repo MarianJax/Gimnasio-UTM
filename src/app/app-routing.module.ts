532e05d07378e3c30c29ad77ce310d2e7b4662ee
MarianJax <mparrales2018@utm.edu.ec>
2024-12-22 18:23:49 -0500

    Eliminar componentes y archivos obsoletos para simplificar la estructura del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,54 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule,Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
-import { AgendamientoModule } from './agendamiento/agendamiento.module';
-import { InventarioModule } from './inventario/inventario.module';
-import { ApiService } from './services/api.service';
-import { AuthGuard } from './guards/auth.guard';
-import { UsuarioModule } from './usuario/usuario.module';
-import { CoreModule } from './core/core.module';
-import { AdmiModule } from './admi/admi.component.module';
-import { AuthModule } from './auth/auth.module';
+import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './component/notfound/notfound.component';
-import { LoginComponent } from './auth/login/login.component';
-
-
+import { LoginComponent } from './pages/auth/login/login.component';
 
 const routes: Routes = [
-  
-    { path: '', pathMatch: 'full', redirectTo: 'auth/login' },
-    { path: 'login', component: LoginComponent }
-
-          
-  {
-    path: 'Inicio',
-    loadChildren: () => import('./agendamiento/agendamiento.module').then(m => m.AgendamientoModule),
-    canActivate: [false]
-  },
-  {
-    path: 'Entrenamiento',
-    loadChildren: () => import('./agendamiento/agendamiento.module').then(m => m.AgendamientoModule),
-  },
-  {
-    path: 'Agendamiento',
-    loadChildren: () => import('./inventario/inventario.module').then(m => m.InventarioModule),
-  },
-  {
-    path: 'Membresia',
-    loadChildren: () => import('./agendamiento/agendamiento.module').then(m => m.AgendamientoModule),
-  },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-            { path: 'notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: '/notfound' },
-  {
-    path: '',
-    redirectTo: 'Login',
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: 'Login',
-  }
+  { path: '', redirectTo: 'Login', pathMatch: 'full' },
+  { path: 'Login', component: LoginComponent },
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin-routing.module').then(m => m.AdminRoutingModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'notfound', component: NotfoundComponent },
+  { path: '**', redirectTo: '/notfound' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AgendamientoModule } from './agendamiento/agendamiento.module';
-import { InventarioModule } from './inventario/inventario.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { LayoutComponent } from './layout/layout.component';
-import { AuthModule } from './auth/auth.module';
+import { AdminRoutingModule } from './pages/admin/admin-routing.module';
 
 @NgModule({
-
-
 
   imports: [BrowserModule, AppRoutingModule],
 
@@ -19,9 +13,9 @@ import { AuthModule } from './auth/auth.module';
   bootstrap: [AppComponent],
 
   declarations: [
-    
+
   ],
 
 })
 
-export class AppModule {}
+export class AppModule { }
