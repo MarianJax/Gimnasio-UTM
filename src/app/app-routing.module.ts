36b6723f9524cc64fb0e8aecd8dd114d38ad3b62
MarianJax <mparrales2018@utm.edu.ec>
2024-12-22 20:04:36 -0500

    Actualizar rutas de autenticación y agregar contenido al componente de inicio de sesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,24 +6,14 @@ import { LoginComponent } from './pages/login/login.component';
 const routes: Routes = [
   { path: '', redirectTo: 'Login', pathMatch: 'full' },
   { path: 'Login', component: LoginComponent },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./pages/admin/admin-routing.module').then(
-        (m) => m.AdminRoutingModule
-      ),
-  },
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('./pages/auth/auth.module').then((m) => m.AuthModule),
-  },
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin-routing.module').then(m => m.AdminRoutingModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
   { path: 'notfound', component: NotfoundComponent },
   { path: '**', redirectTo: '/notfound' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
