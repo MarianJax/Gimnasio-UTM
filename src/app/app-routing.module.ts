1b6e3c0beb0ab95348ce6ed7e887f66187ebcfb5
MarianJax <mparrales2018@utm.edu.ec>
2024-12-23 21:56:54 -0500

    Actualizar rutas de autenticación para redirigir a '/auth/login' y eliminar código obsoleto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,6 @@ import { LoginModule } from './pages/auth/login/login.module';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  { path: '', redirectTo: 'Login', pathMatch: 'full' },
-  { path: 'Login', component: LoginComponent },
-  { path: 'admin', loadChildren: () => import('./pages/admin/admin-routing.module').then(m => m.AdminRoutingModule) },
-  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'notfound', component: NotfoundComponent },
-  { path: '**', redirectTo: '/notfound' },
-=======
   {
     path: '',
     redirectTo: '/auth/login',
@@ -33,15 +25,10 @@ const routes: Routes = [
       }
     ]
   }
->>>>>>> d2286eaf11fd606b53e2c8287a4f83c3ed20d854
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
 export class AppRoutingModule { }
-=======
-export class AppRoutingModule { }
->>>>>>> d2286eaf11fd606b53e2c8287a4f83c3ed20d854
