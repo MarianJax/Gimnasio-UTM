c51bcaed01250d386c6303603c816ddf0b79786a
MarianJax <mparrales2018@utm.edu.ec>
2024-12-28 15:29:19 -0500

    Add Home and Admin modules with routing setup and update app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginModule } from './pages/auth/login/login.module';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/auth/login',
-    pathMatch: 'full'
-  },
   {
     path: 'auth',
     children: [
@@ -22,9 +18,18 @@ const routes: Routes = [
       {
         path: '',
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'admin',
+        loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
       }
     ]
-  }
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { AdminRoutingModule } from './pages/admin/admin-routing.module';
+import { HomeRoutingModule } from './pages/home/home-routing.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     TopBarComponent
   ],
   imports: [
