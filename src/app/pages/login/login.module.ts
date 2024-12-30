63f6589d559bc483ad563aef31de94c7c7796593
MarianJax <mparrales2018@utm.edu.ec>
2024-12-30 10:21:19 -0500

    Refactor authentication module: move login component to new path and update routing

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    ReactiveFormsModule, 
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    ToastModule,
+    RippleModule
+  ]
+})
+export class LoginModule { }
+
