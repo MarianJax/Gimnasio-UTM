9c06b4439faa694bc63a387742646b767f109d75
MarianJax <mparrales2018@utm.edu.ec>
2025-01-03 15:49:08 -0500

    Update footer component to support dynamic logo based on color scheme and implement theme management

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,5 +6,23 @@ import { LayoutService } from '../service/layout.service';
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
-  constructor(public layoutService: LayoutService) { }
+  theme: any;
+  colorScheme: any;
+
+  constructor(public layoutService: LayoutService) {
+    this.theme = localStorage.getItem('theme_utm_gimnasio');
+    this.colorScheme = localStorage.getItem('color_scheme_utm_gimnasio');
+    if (this.theme) {
+      this.changeTheme(this.theme, this.colorScheme);
+    } else {
+      this.theme = "saga-green";
+      this.colorScheme = "light";
+    }
+  }
+
+  changeTheme(theme: string, colorScheme: string) {
+    const themeLink = <HTMLLinkElement>document.getElementById('theme-css');
+    const newHref = themeLink.getAttribute('href')!.replace(this.layoutService.config.theme, theme);
+    this.layoutService.config.colorScheme
+  }
 }
