586bf63c83a96ac90803cacc36084ccd526911e3
MarianJax <mparrales2018@utm.edu.ec>
2025-01-07 15:57:45 -0500

    Actualizar la presentación y lógica del componente de detalle de rutina, mejorando estilos y añadiendo nuevas rutinas de ejercicios

--- src/app/pages/home/rutina-detail/rutina-detail.component.ts
+++ src/app/pages/home/rutina-detail/rutina-detail.component.ts
@@ -134,7 +134,135 @@ export class RutinaDetailComponent implements OnInit {
           peso:'10kg',
         },
       ]
-    }
+
+    },
+    {
+      slug: 'gluteo',
+      machines: [
+        {
+          id: 1,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 10,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 2,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 20,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 3,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 30,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 4,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 40,
+          maquina: 'Maquina',
+          peso:'10kg',
+
+        },
+      ],
+    },
+
+    {
+      slug: 'espalda-biceps',
+      machines: [
+        {
+          id: 1,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 10,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 2,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 20,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 3,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 30,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 4,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 40,
+          maquina: 'Maquina',
+          peso:'10kg',
+
+        },
+      ],
+    },
+    {
+      slug: 'hombros-abdomen',
+      machines: [
+        {
+          id: 1,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 10,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 2,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 20,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 3,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 30,
+          maquina: 'Maquina',
+          peso:'10kg',
+        },
+        {
+          id: 4,
+          nombre: 'Calentamiento',
+          descripcion: 'Ejercicios de calentamiento',
+          estado: 'Disponible',
+          duracion: 40,
+          maquina: 'Maquina',
+          peso:'10kg',
+
+        },
+      ],
+    },
   ];
 
   constructor(private route: ActivatedRoute, private router: Router) {}
