a5d159e0acb75b199949c26bd2ddd7837be21991
MarianJax <mparrales2018@utm.edu.ec>
2025-01-09 19:29:03 -0500

    Implement Ejercicios component with form for exercise registration and display of registered exercises

--- src/app/pages/admin/ejercicios/ejercicios.component.ts
+++ src/app/pages/admin/ejercicios/ejercicios.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AutoCompleteCompleteEvent } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-ejercicios',
   templateUrl: './ejercicios.component.html',
   styleUrls: ['./ejercicios.component.scss']
 })
-export class EjerciciosComponent {
+export class EjerciciosComponent implements OnInit {
+  myGroup: FormGroup;
+
+  filtrosRutinas: any[] = [];
+  suggestions: any[] | undefined;
+  isEmpty: boolean = false;
+
+  // deberia ser un service de la base de datos
+  rutinas = [
+    {
+      name: 'Pecho', code: 'pecho'
+    },
+    {
+      name: 'Pierna', code: 'pierna'
+    },
+    {
+      name: 'Brazos', code: 'brazos'
+    },
+  ]
+
+  // ejercicios que debe ser un service de la base de datos
+  ejerciciosData = [
+    { 
+      maquina: 'Cinta de Correr',
+      estado: 'Disponible',
+      ejercicio: {
+        nombre: 'No sé el nombre',
+      }
+     },
+     {
+       maquina: 'Bicicleta Estática',
+       estado: 'Disponible',
+       ejercicio: {
+         nombre: 'No sé el nombre',
+       }
+     },
+     {
+       maquina: 'Máquina de Remo',
+       estado: 'Disponible',
+       ejercicio: {
+         nombre: 'No sé el nombre',
+       }
+     }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.myGroup = this.fb.group({
+      cantidad: new FormControl<string | null>(null),
+      nombre: new FormControl<string | null>(null),
+      usuario: new FormControl<string | null>(null),
+      descripcion: new FormControl<string | null>(null)
+    });
+  }
+
+  filtroRutinas(event: AutoCompleteCompleteEvent) {
+    let filtered: any[] = [];
+    let query = event.query;
+
+    for (let i = 0; i < (this.rutinas as any[]).length; i++) {
+      let ejerci = (this.rutinas as any[])[i];
+      if (ejerci.name && ejerci.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(ejerci);
+      }
+    }
+
+    this.isEmpty = filtered.length === 0;
+    this.filtrosRutinas = filtered;
+  }
+
+  ngOnInit(): void { }
 
 }
--- src/app/pages/admin/ejercicios/ejercicios.module.ts
+++ src/app/pages/admin/ejercicios/ejercicios.module.ts
@@ -3,7 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { EjerciciosRoutingModule } from './ejercicios-routing.module';
 import { EjerciciosComponent } from './ejercicios.component';
-
+import { IconModule } from 'src/app/components/icons/icons.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,15 @@ import { EjerciciosComponent } from './ejercicios.component';
   ],
   imports: [
     CommonModule,
-    EjerciciosRoutingModule
+    EjerciciosRoutingModule,
+        IconModule,
+        FormsModule,
+        ButtonModule,
+        RippleModule,
+        ReactiveFormsModule,
+        InputTextModule,
+        InputTextareaModule,
+        AutoCompleteModule
   ]
 })
 export class EjerciciosModule { }
