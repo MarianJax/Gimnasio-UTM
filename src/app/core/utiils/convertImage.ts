85abdb29e8255acc207ea348839ba52c90e9e9b1
MarianJax <mparrales2018@utm.edu.ec>
2025-04-08 13:33:04 -0500

    feat: Enhance image handling and styling with improved promise structure and focus effects

--- src/app/core/utiils/convertImage.ts
+++ src/app/core/utiils/convertImage.ts
@@ -1,17 +1,18 @@
 export function imageToArrayBuffer(input: HTMLInputElement) {
-  return new Promise((resolve, reject) => {
+  return new Promise<{ buffer: ArrayBuffer; mimeType: string }>((resolve, reject) => {
     const file = input.files ? input.files[0] : null; // Obtener el archivo seleccionado
 
-      if (!file) {
-        reject("No file selected");
-        return;
-      }
+    if (!file) {
+      reject("No file selected");
+      return;
+    }
 
     const reader = new FileReader();
 
     reader.onloadend = function () {
-      const arrayBuffer = reader.result; // El resultado es un ArrayBuffer
-      resolve(arrayBuffer);
+      const arrayBuffer = reader.result as ArrayBuffer; // El resultado es un ArrayBuffer
+      const mimeType = file.type; // Obtener el tipo MIME del archivo
+      resolve({ buffer: arrayBuffer, mimeType }); // Retornar ambos valores
     };
 
     reader.onerror = function () {
@@ -20,4 +21,10 @@ export function imageToArrayBuffer(input: HTMLInputElement) {
 
     reader.readAsArrayBuffer(file);
   });
+}
+
+export function bufferToImage(buffer: any, type: string): string {
+  const uint8Array = new Uint8Array(buffer);
+    const binaryString = uint8Array.reduce((data, byte) => data + String.fromCharCode(byte), '');
+    return `data:${type};base64,${btoa(binaryString)}`;
 }
