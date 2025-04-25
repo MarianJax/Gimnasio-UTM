3ac47d0b362a0a0d599c59906a2e1ef4037f84fc
MarianJax <mparrales2018@utm.edu.ec>
2025-04-24 20:39:47 -0500

    fix(usuarios): update API URL to correct endpoint for user data retrieval

--- src/app/service/usuarios/usuarios.service.ts
+++ src/app/service/usuarios/usuarios.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UsuariosService {
-  private apiUrl = 'http://localhost:3000/usuario';
+  private apiUrl = 'http://localhost:3000/persona';
   constructor(private http: HttpClient) { }
 
   obtenerUsuarios(): Observable<any> {
