e5c8ebaee228233d1ee2163c85c522b8d9bcec46
MarianJax <mparrales2017@utm.edu.ec>
2025-04-27 04:30:12 -0500

    feat: agregar ícono SVG de dinero y optimizar imports en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { MenuModule } from 'primeng/menu';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { IconModule } from './components/icons/icons.module';
-
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
--- src/app/guards/has-roles.guard.ts
+++ src/app/guards/has-roles.guard.ts
@@ -1,11 +1,13 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../pages/login/auth.service';
-import { inject } from '@angular/core';
 
 export const hasRolesGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const expectedRole: string[] = route.data['expectedRole'];
   // Check if the user is authenticated
-  const userRoles = authService.getUserData().roles;
-  return expectedRole.some(role => userRoles.includes(role));
+  const userRoles = authService.getUserData().rol;
+  return expectedRole.some((role) => userRoles.includes(role));
 };
+
+['DOCENTE', 'DOCENTE TIPO 2', 'ESTUDIANTE'];
